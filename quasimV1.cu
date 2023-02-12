35e86ca0e9bd38b49507330f12efeeb94c3156a5
Hrushikesh Patil <hrushikeshpatil890@gmail.com>
2023-02-12 15:34:18 -0500

    Cudamemcpy working

--- quasimV1.cu
+++ quasimV1.cu
@@ -57,15 +57,24 @@ float *fetch_data(float u[4], char* filename, int &qno, int &vec_length)
 }
 
 __global__ void
-quantum_gate_multiply(int a, int b)
+quantum_gate_multiply(float* u, float* vec, float* op, int qno, int vec_length, int pw, int mask, int antimask)
 {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
-    printf("%d %d i= %d\n", a, b, i);
+    int idx1, idx2;
+
+    idx1 = (i & mask) | ((i & antimask) << 1);
+    idx2 = (idx1 | (1 << qno));
+
+    op[idx1] = u[0] * vec[idx1] + u[1] * vec[idx2];
+    op[idx2] = u[2] * vec[idx1] + u[3] * vec[idx2];
+    
+    //printf("%d %d %d\n", i, idx1, idx2);
 }
 
 
 int main(int argc, char *argv[])
 {
+    cudaError_t err = cudaSuccess;
     float u[4];
     
     int qno, vec_length;
@@ -87,22 +96,83 @@ int main(int argc, char *argv[])
 
     antimask = (antimask & (~mask));
 
-    /*for (int j = 0; j < vec_length; j++)
+    // Code below taken from class vecAdd code
+    size_t size = vec_length * sizeof(float);
+    float *d_u = NULL;
+    err = cudaMalloc((void **)&d_u, (4 * sizeof(float)));
+
+    if (err != cudaSuccess)
     {
-        printf("%f\n", vec[j]);
-    }*/
-    int threadsPerBlock = 256;
-    int blocksPerGrid =(vec_length + threadsPerBlock - 1) / threadsPerBlock;
-    printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
+        fprintf(stderr, "Failed to allocate device vector u (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    float *d_vec = NULL;
+    err = cudaMalloc((void **)&d_vec, size);
+
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to allocate device vector vec (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    float *d_op = NULL;
+    err = cudaMalloc((void **)&d_op, size);
+
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to allocate device vector op (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+    
+    // Cudamemcpy
+
+    err = cudaMemcpy(d_u, u, (4 * sizeof(float)), cudaMemcpyHostToDevice);
+
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to copy vector A from host to device (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaMemcpy(d_vec, vec, size, cudaMemcpyHostToDevice);
+
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to copy vector B from host to device (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    int threadsPerBlock = 2;
+    int blocksPerGrid =((vec_length/2) + threadsPerBlock - 1) / threadsPerBlock;
+    
+    //printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
     //vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, numElements);
 
     //quantum_gate_multiply(u, vec, op, qno, vec_length, pw, mask, antimask);
-    quantum_gate_multiply<<<blocksPerGrid, threadsPerBlock>>>(qno, vec_length);
     
-    /*for (int j = 0; j < vec_length; j++)
+    quantum_gate_multiply<<<blocksPerGrid, threadsPerBlock>>>(d_u, d_vec, d_op, qno, vec_length, pw, mask, antimask);
+    
+    err = cudaGetLastError();
+
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to launch vectorAdd kernel (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaMemcpy(op, d_op, size, cudaMemcpyDeviceToHost);
+
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to copy vector C from device to host (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    for (int j = 0; j < vec_length; j++)
     {
         printf("%.3f\n", op[j]);
-    }*/
+    }
 
     free(vec);
     return 1;
